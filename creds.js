e06ae2e04d4406c68f1e08de53c7facfbf189e69
AdrianSkar <adrian@adrianskar.com>
2021-09-29 13:50:59 +0200

    update[script]: formatting + prep for other side projects

--- creds.js
+++ creds.js
@@ -1,22 +1,22 @@
 // Clockify credentials
 const creds = {
-	apiKey: "yourApiKey",
-	workspace: "yourWorkspaceId",
-	userId: "yourUserId"
+	apiKey: 'yourApiKey',
+	workspace: 'yourWorkspaceId',
+	userId: 'yourUserId',
 };
 const targets = {
-	"workweek": { default: 2400, maxRatio: 1.25 }, // week's target in minutes + maxRatio
-	"ej": { default: 600, recovery: -5 }, // 10h min. project
-	"lbt": 1200,
-	"w": { default: 600, ratio: 0.23, recovery: -292 }, // default target and ratio if default doesn't apply (other higher priority projects)
-	"pract": { default: 1104, ratio: 0.51, recovery: 277 },
-	"lb": { default: 48, ratio: 0.02, recovery: -221 },
-	"p": { default: 288, ratio: 0.12, recovery: -918 },
-	"eng": { default: 360, ratio: 0.12, recovery: 0 },
-	// recovery is a modifier that involves previous surpluses of times and is 
-	// used to adjust targets accordingly 
+	workweek: { default: 2400, maxRatio: 1.25 }, // week's target in minutes + maxRatio
+	ej: { default: 600, recovery: -5 }, // 10h min. project
+	lbt: 1200,
+	w: { default: 600, ratio: 0.23, recovery: -292 }, // default target and ratio if default doesn't apply (other higher priority projects)
+	pract: { default: 1104, ratio: 0.51, recovery: 277 },
+	lb: { default: 48, ratio: 0.02, recovery: -221 },
+	p: { default: 288, ratio: 0.12, recovery: -918 },
+	eng: { default: 360, ratio: 0.12, recovery: 0 },
+	// recovery is a modifier that involves previous surpluses of times and is
+	// used to adjust targets accordingly
 	pendingColor: 'firebrick',
 	recoveryColor: 'coral',
-	doneColor: 'forestgreen'
+	doneColor: 'forestgreen',
 };
 export { creds, targets };
--- script.js
+++ script.js
@@ -11,10 +11,22 @@ const getMonday = () => {
 };
 
 const monday = getMonday(),
-	start = new Date(Date.UTC(monday.getFullYear(), monday.getMonth(), monday.getDate(), 0, 0, 0)).toISOString(); // Mon, 00:00:00 (UTC/ISO)
+	start = new Date(
+		Date.UTC(monday.getFullYear(), monday.getMonth(), monday.getDate(), 0, 0, 0)
+	).toISOString(); // Mon, 00:00:00 (UTC/ISO)
 
 const today = new Date(),
-	end = new Date(Date.UTC(today.getFullYear(), today.getMonth(), today.getDate(), 23, 59, 59, 999)).toISOString(); // Today, 23:59:59 (UTC/ISO)
+	end = new Date(
+		Date.UTC(
+			today.getFullYear(),
+			today.getMonth(),
+			today.getDate(),
+			23,
+			59,
+			59,
+			999
+		)
+	).toISOString(); // Today, 23:59:59 (UTC/ISO)
 
 /// Add week number to page title
 // https://weeknumber.net/how-to/javascript
@@ -23,15 +35,24 @@ Date.prototype.getWeek = function () {
 	var date = new Date(this.getTime());
 	date.setHours(0, 0, 0, 0);
 	// Thursday in current week decides the year.
-	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+	date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
 	// January 4 is always in week 1.
 	var week1 = new Date(date.getFullYear(), 0, 4);
 	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
-	const result = 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-		- 3 + (week1.getDay() + 6) % 7) / 7);
+	const result =
+		1 +
+		Math.round(
+			((date.getTime() - week1.getTime()) / 86400000 -
+				3 +
+				((week1.getDay() + 6) % 7)) /
+				7
+		);
 	return result.toString().padStart(2, '0');
 };
-document.querySelector('h1').textContent += ` (${today.getFullYear().toString().slice(2)}${today.getWeek()})`;
+document.querySelector('h1').textContent += ` (${today
+	.getFullYear()
+	.toString()
+	.slice(2)}${today.getWeek()})`;
 
 //______________________________________________________________________________
 
@@ -40,31 +61,30 @@ const durMinutes = val => Math.round(val / 60),
 	getId = val => document.getElementById(val);
 let myList = document.querySelector('ul'),
 	// Variables holding fetched data and default target times
-	ej = targets.ej.default, lbt = targets.lbt,
+	ej = targets.ej.default,
+	lbt = targets.lbt,
 	style = document.getElementsByTagName('style')[0];
 fetch(
-	`https://reports.api.clockify.me/v1/workspaces/${creds.workspace}/reports/summary`, {
-	method: 'POST',
-	headers: {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json',
-		"X-Api-Key": `${creds.apiKey}`
-	},
-	body: JSON.stringify({
-		"dateRangeStart": start,
-		"dateRangeEnd": end,
-		"summaryFilter": {
-			"groups": [
-				"project",
-				"timeentry"
-			]
-		}
-	})
-}
+	`https://reports.api.clockify.me/v1/workspaces/${creds.workspace}/reports/summary`,
+	{
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			'X-Api-Key': `${creds.apiKey}`,
+		},
+		body: JSON.stringify({
+			dateRangeStart: start,
+			dateRangeEnd: end,
+			summaryFilter: {
+				groups: ['project', 'timeentry'],
+			},
+		}),
+	}
 )
 	.then(function (response) {
 		if (!response.ok) {
-			throw new Error("HTTP error, status = " + response.status);
+			throw new Error('HTTP error, status = ' + response.status);
 		}
 		return response.json();
 	})
@@ -76,68 +96,82 @@ fetch(
 		let lbtDur;
 		for (let entry of data.groupOne) {
 			if (entry.name.includes('Lb')) {
-				lbtDur = durMinutes(entry.duration); // Duration in mins of lbt project 
+				lbtDur = durMinutes(entry.duration); // Duration in mins of lbt project
 			}
 			break;
 		}
-		let ww = targets.workweek.default, freelance;
+		let ww = targets.workweek.default,
+			side,
+			side_pract;
 
 		const taskTargets = function (val) {
-			if (lbtDur > 0) {// If there's record of lbt; calculate accordingly
+			if (lbtDur > 0) {
+				// If there's record of lbt; calculate accordingly
 				return Math.round((ww - lbtDur) * targets[val].ratio);
-			}// If lbtDur surpasses workweek set to 0, otherwise load defaults
-			return (lbtDur >= ww) ? 0 : Math.round(ww * targets[val].ratio);
+			} // If lbtDur surpasses workweek set to 0, otherwise load defaults
+			return lbtDur >= ww ? 0 : Math.round(ww * targets[val].ratio);
 		};
 		const mainTasks = {
 			w: taskTargets('w'),
 			pract: taskTargets('pract'),
 			lb: taskTargets('lb'),
 			p: taskTargets('p'),
-			eng: taskTargets('eng')
+			eng: taskTargets('eng'),
 		};
 
 		/// Return color depending on results:
 		const colorize = function (left, rec) {
 			let recLeft = left + rec;
-			return (recLeft >= 0) ? ((left >= 0) ? targets.pendingColor : targets.recoveryColor) : targets.doneColor;
+			return recLeft >= 0
+				? left >= 0
+					? targets.pendingColor
+					: targets.recoveryColor
+				: targets.doneColor;
 		};
 
-		//__________________________________________________________________________
+		//______________________________________________________________________________
 
 		/// Calculate % done
 		let percentageDone = (taskName, time, taskTime) => {
-			let value = Math.round((time * 100) / (taskTime + targets[taskName].recovery));
-			if ((taskTime + targets[taskName].recovery) < 0) {
+			let value = Math.round(
+				(time * 100) / (taskTime + targets[taskName].recovery)
+			);
+			if (taskTime + targets[taskName].recovery < 0) {
 				return 100;
 			}
-			return value > 0 ? value > 100 ? 100 : value : 0;
+			return value > 0 ? (value > 100 ? 100 : value) : 0;
 		};
 
-		//__________________________________________________________________________
-
+		//______________________________________________________________________________
 
 		/// Loop through projects
 		for (const value in data.groupOne) {
-			let calc, content,
+			let calc,
+				content,
 				frag = document.createDocumentFragment(),
 				duration = data.groupOne[value].duration,
 				time = durMinutes(duration);
 
-			if (data.groupOne[value].name.includes('Ej')) {//* Process Ej project
+			if (data.groupOne[value].name.includes('Ej')) {
+				//* Process Ej project
 				const ejTR = getId('ej');
 				calc = ej - time;
 				content = [`${time}`, `${ej}\'`, `${calc}`];
-				for (let i = 0, y = content.length, td; i < y; i++) {// Append content as td elements
+				for (let i = 0, y = content.length, td; i < y; i++) {
+					// Append content as td elements
 					td = document.createElement('td');
 					td.innerHTML = content[i];
 					frag.appendChild(td);
 				}
 				ejTR.appendChild(frag);
-				ejTR.setAttribute('title', `target + recovery: ${ej + targets.ej.recovery}`);
+				ejTR.setAttribute(
+					'title',
+					`target + recovery: ${ej + targets.ej.recovery}`
+				);
 				ejTR.lastChild.style.color = colorize(calc, targets.ej.recovery);
 
 				// progress bar
-				ejTR.className = "named bars";
+				ejTR.className = 'named bars';
 				style.innerHTML += `
 				#ej::after {
 					width: ${percentageDone('ej', time, ej)}% !important;
@@ -146,14 +180,14 @@ fetch(
 				`;
 				document.head.appendChild(style);
 				//______________________________________________________________________
-			}
-			else if (data.groupOne[value].name.includes('Lb')) {//* Process Lbt project
+			} else if (data.groupOne[value].name.includes('Lb')) {
+				//* Process Lbt project
 				const lbTR = getId('lbt');
 				calc = lbt - time;
 
 				content = [`${time}`, `${lbt}\'`, `${calc}`];
 				for (let x = 0, y = content.length, td; x < y; x++) {
-					td = document.createElement("td");
+					td = document.createElement('td');
 					td.innerHTML = content[x];
 					frag.appendChild(td);
 				}
@@ -162,12 +196,13 @@ fetch(
 				lbTR.lastChild.style.color = colorize(calc, 0);
 
 				// progress bar
-				let percentageLBT = (time, taskTime) => { // redefined because lbt doesn't require recovery
+				let percentageLBT = (time, taskTime) => {
+					// redefined because lbt doesn't require recovery
 					let value = Math.round((time * 100) / taskTime);
-					return value > 0 ? value > 100 ? 100 : value : 0;
+					return value > 0 ? (value > 100 ? 100 : value) : 0;
 				};
 
-				lbTR.className = "named bars";
+				lbTR.className = 'named bars';
 				style.innerHTML += `
 								#lbt::after {
 									width: ${percentageLBT(time, lbt)}% !important;
@@ -176,34 +211,33 @@ fetch(
 								`;
 				document.head.appendChild(style);
 				//______________________________________________________________________
-
-			}
-			else if (data.groupOne[value].name.includes('Freelance')) {// Process freelance projects as 'pract' next.
-				freelance = time;
-
-			}
-
-			else {//* Process main project tasks
+			} else if (data.groupOne[value].name.includes('side_pract')) {
+				// Process freelance/side projects related to pract as 'pract' next.
+				side_pract = time;
+			} else {
+				//* Process main project tasks
 				const tasks = data.groupOne[value].children;
 				let mainArr = ['pract', 'w', 'eng', 'p', 'lb'],
 					contentArr = [],
 					table = document.getElementById('table');
+				// let perDone = Math.round((time * 100) / (taskTime + targets[task.name].recovery));
 
-				/// Output function 
+				/// Output function
 
 				const mainTask = function (task) {
 					const taskTime = mainTasks[task.name]; //  "|| targets[task.name].default" removed: default is already assign on mainTasks
 					let time = durMinutes(task.duration) || 0;
-					if (task.name === 'pract') {// Include freelance time to 'pract'
-						time += freelance ? freelance : 0;
+					if (task.name === 'pract') {
+						// Include freelance time to 'pract'
+						time += side_pract ? side_pract : 0;
 					}
 					let tr = document.createElement('tr');
 
 					calc = taskTime - time;
 					content = [task.name, `${time}`, `${taskTime}`, `${calc}`];
+					tr.className = `named bars ${task.name}`;
 
 					// progress bar
-					tr.className = `named bars ${task.name}`;
 					style.innerHTML += `
 					.${task.name}::after {
 						width: ${percentageDone(task.name, time, taskTime)}% !important;
@@ -211,16 +245,22 @@ fetch(
 					}
 					`;
 					document.head.appendChild(style);
-					//____________________________________________________________________
+					//______________________________________________________________________
 
-					tr.setAttribute('title', `target + recovery: ${taskTime + targets[task.name].recovery}`);
+					tr.setAttribute(
+						'title',
+						`target + recovery: ${taskTime + targets[task.name].recovery}`
+					);
 
 					for (let i = 0, y = content.length, td; i < y; i++) {
 						td = document.createElement('td');
 						td.innerHTML = content[i];
 						tr.appendChild(td);
 					}
-					tr.lastChild.style.color = colorize(calc, targets[task.name].recovery);
+					tr.lastChild.style.color = colorize(
+						calc,
+						targets[task.name].recovery
+					);
 
 					let toContent = {};
 					toContent.name = task.name;
@@ -229,7 +269,9 @@ fetch(
 				};
 				/// Iterate over fetched main tasks and apply the output function
 
-				for (let task of tasks) { mainTask(task); }
+				for (let task of tasks) {
+					mainTask(task);
+				}
 
 				/// Output main tasks for which there's no logged time yet
 
@@ -256,19 +298,15 @@ fetch(
 
 				/// Output content
 				table.appendChild(frag);
-
 			}
-
 		}
 		//__________________________________________________________________________
 
-
-		/// Process total count 
+		/// Process total count
 
 		const targetHours = Math.round((ww + ej) / 60), //ej project is counted apart from main ones
 			maxHours = Math.round(targetHours * targets.workweek.maxRatio),
 			total = durHours(data.totals[0].totalTime),
-
 			listItem = document.createElement('li'),
 			left = `<span id="totalLeft">${(targetHours - total).toFixed(2)}</span>`;
 
@@ -277,15 +315,15 @@ fetch(
 		myList.appendChild(listItem);
 
 		let totalLeft = document.getElementById('totalLeft');
-		totalLeft.style.color = colorize((targetHours - total), (maxHours - targetHours));
+		totalLeft.style.color = colorize(
+			targetHours - total,
+			maxHours - targetHours
+		);
 
 		//__________________________________________________________________________
-
 	})
 	.catch(function (error) {
 		let p = document.createElement('p');
-		p.appendChild(
-			document.createTextNode('Error: ' + error.message + 'stack: ' + error.stack)
-		);
+		p.appendChild(document.createTextNode('Error: ' + error.message));
 		document.body.insertBefore(p, myList);
 	});
